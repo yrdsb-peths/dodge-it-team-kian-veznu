7ec4af3490209afe5bc912cde4ca44c7c83cc6e3
348588096 <veznup@gmail.com>
2025-04-22 14:18:04 -0400

    Added a Rock to Screen

--- MyWorld.java
+++ MyWorld.java
@@ -3,7 +3,11 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
         Hero hero = new Hero();
         addObject(hero, 0, 200);
+        
+        Rock rock = new Rock();
+        addObject(rock,300,200);
     }
 }
