9bbf47e766c1804515c69b50c6432a6387665380
348586678 <348586678@gapps.yrdsb.ca>
2025-04-22 14:22:22 -0400

    updated hero location

--- MyWorld.java
+++ MyWorld.java
@@ -5,7 +5,7 @@ public class MyWorld extends World {
         super(600, 400, 1);
         
         Hero hero = new Hero();
-        addObject(hero, 200, 500);
+        addObject(hero, 200, 250);
         
         Rock rock = new Rock();
         addObject(rock,300,200);
