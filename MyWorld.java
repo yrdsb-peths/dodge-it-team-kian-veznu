1c5f0f4f5e1aecf712249e4fad40292f9a7256bb
348586678 <348586678@gapps.yrdsb.ca>
2025-04-22 14:21:43 -0400

    fixed conflicts

--- MyWorld.java
+++ MyWorld.java
@@ -5,12 +5,10 @@ public class MyWorld extends World {
         super(600, 400, 1);
         
         Hero hero = new Hero();
-<<<<<<< HEAD
-        addObject(hero, 0, 500);
-=======
+        addObject(hero, 200, 500);
         
         Rock rock = new Rock();
         addObject(rock,300,200);
->>>>>>> 7ec4af3490209afe5bc912cde4ca44c7c83cc6e3
+
     }
 }
