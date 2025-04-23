8b1166f03ef7a5163a9726511651dab7b1370fc1
348586678 <348586678@gapps.yrdsb.ca>
2025-04-23 14:06:01 -0400

    Updated rock code

--- Rock_1.java
+++ Rock_1.java
@@ -1,7 +1,7 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Rock_1 here.
+ * Write a description of class Cactus here.
  * 
  * @author (your name) 
  * @version (a version number or a date)
@@ -9,11 +9,35 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class Rock_1 extends Actor
 {
     /**
-     * Act - do whatever the Rock_1 wants to do. This method is called whenever
+     * Act - do whatever the Cactus wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
     public void act()
     {
-        // Add your action code here.
+        move(-7);
+        
+
+        if (getX() <= 0) {
+            resetRock_1();
+        }
+        
+        if (isTouching(Hero.class)){
+            Skull skull = new Skull();
+            getWorld().addObject(skull, 300, 200);
+            getWorld().removeObject(this);
+        }
+    }
+    
+    public void resetRock_1()
+    {
+        int num = Greenfoot.getRandomNumber(3);
+        if (num == 1){
+            setLocation(600, 200);
+        } else if (num == 2){
+            setLocation(600, 100);
+        } else {
+            setLocation (600, 0);
+        }
+
     }
 }
