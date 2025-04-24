4696dfb6f59a8b9c4012598b14641246d97c9a53
348586678 <348586678@gapps.yrdsb.ca>
2025-04-24 13:41:14 -0400

    Added Counter actor

--- MyWorld.java
+++ MyWorld.java
@@ -3,18 +3,27 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
-        
+
         Hero hero = new Hero();
         addObject(hero, 300, 200);
-        
+
         Rock rock = new Rock();
         addObject(rock,0,0);
-        
+
         Rock_1 rock1 = new Rock_1();
         addObject(rock1,0,300);
-        
+
         GreenfootImage background = new GreenfootImage("grass.jpg");
         setBackground(background);
-        
+
+        prepare();
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
     }
 }
