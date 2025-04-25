710e78856ee7b90a888a270423f0eceb6b3f6051
348586678 <348586678@gapps.yrdsb.ca>
2025-04-25 14:46:12 -0400

    Fixed rock

--- TopRock.java
+++ TopRock.java
@@ -4,11 +4,33 @@ public class TopRock extends Actor
 {
     public void act()
     {
-        move(-2);
+        move(6);
         
-        if(getX()==0)
-        {
-            setLocation(600,100);
+        if (getX() <= 0) {
+            resetHero();
+        }
+        
+        if (isTouching(Hero.class)){
+            Skull skull = new Skull();
+            getWorld().addObject(skull, 300, 200);
+            getWorld().removeObject(this);
+        }
+    }
+    
+    public void resetHero()
+    {
+        int num = Greenfoot.getRandomNumber(4);
+        if (num == 0){
+            setLocation(600, 100);
+        } else if (num == 1) {
+            setLocation(600, 200);
+        } else if (num == 2){
+            setLocation(600, 300);
+        } else if (num == 3){
+            setLocation(600, 400);
+        } else {
+            setLocation(600, 500);
         }
     }
 }
+
